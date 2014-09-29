6900c1f83223e1abdbf7368c8c8c452f9108714f
Edgar Zanella Alvarenga <e@vaz.io>
2014-09-29 14:54:23 -0300

    Adding parameters to difference visualisation types MorphRender

--- openFrameworks/kinectTracking/src/ofMorph.cpp
+++ openFrameworks/kinectTracking/src/ofMorph.cpp
@@ -8,6 +8,10 @@ ofMorph::ofMorph(float _x, float _y, float _w, float _h, ofColor _c, int _type)
     h = _h;
     c = _c;
     type = _type;
+
+    for (int i; i < 3; ++i) {
+        screens[i] = (ofRandom(0, 1) >= 0.5)?true:false;
+    }
     
 
 }
--- openFrameworks/kinectTracking/src/ofMorphRender.h
+++ openFrameworks/kinectTracking/src/ofMorphRender.h
@@ -26,6 +26,21 @@ public:
     ofParameter<ofVec2f> position;
     ofParameter<ofColor> color;
     
+    // Tunnel Renderer Parameters
+    ofParameter<float> tunnel_max_width;
+    ofParameter<float> tunnel_min_width;
+    ofParameter<int> tunnel_num_layers;
+    ofParameter<ofColor> tunnel_lower_color;
+    ofParameter<ofColor> tunnel_higher_color;
+
+    // Bars Renderer Parameters
+    ofParameter<float> bars_min_width;
+    ofParameter<float> bars_max_width;
+    
+    // Spikes Parameters
+    ofParameter<int> spikes_min_num;
+    ofParameter<int> spikes_max_num;
+
 };
 
 #endif /* OFMORPHRENDER_H_*/
