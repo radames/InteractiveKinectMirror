ea8ddf48fceeb071a79983347f91e8495ba41f77
Radamés Ajna <radamajna@gmail.com>
2014-10-01 13:57:05 -0300

    fixing the GUI groups, and starting the spikes animation

--- openFrameworks/kinectTracking/src/ofMorph.cpp
+++ openFrameworks/kinectTracking/src/ofMorph.cpp
@@ -1,6 +1,6 @@
 #include "ofMorph.h"
 
-void ofMorph::setup(float _x, float _y)
+void ofMorph::setup(float _x, float _y, int minSpikesRandPoints, int maxSpikesRandPoints, int minNoiseCorner, int maxNoiseCorner)
 {
     x = _x;
     y = _y;
@@ -11,9 +11,23 @@ void ofMorph::setup(float _x, float _y)
         screens[i] = true; (ofRandom(0, 1) >= 0.5)?true:false;
     }
     
+    //sampling the noise for cornes
     for (int i = 0; i < 8; ++ i) {
-        random_delta[i] = ofRandom(10, 50);
+        random_delta[i] = ofRandom(minNoiseCorner, maxNoiseCorner);
     }
+    int r = ofRandom(minSpikesRandPoints, maxSpikesRandPoints);
+    
+    //sampling the randon points in the screen
+    for(int i; i < r; i++){
+        // must be outside the shape
+    //    Ofpoint p;
+        float rx,ry;
+        while( (rx < x-w/2 || rx > x+w/2) && (ry < y-h/2 || ry > y + h/2){
+     ///       rx = ofRandom(0,)
+        }
+        
+    }
+    
     
 }
 
--- openFrameworks/kinectTracking/src/ofMorphRender.cpp
+++ openFrameworks/kinectTracking/src/ofMorphRender.cpp
@@ -15,21 +15,36 @@ ofMorphRender::ofMorphRender() {
 
 void ofMorphRender::setup(ofFbo *_screen1, ofFbo *_screen2, ofFbo *_screen3, float _kinect_width, float _kinect_height){
     
-    render_type = GRADIENT;//RenderType(random() % 3);
+    render_type = SPIKES;//RenderType(random() % 3);
     screen1 = _screen1;
     screen2 = _screen2;
     screen3 = _screen3;
     kinect_width = _kinect_width;
     kinect_height = _kinect_height;
-    parameters.setName("Morphs");
-    parameters.add(size.set("size",10,0,100));
-    parameters.add(number.set("number",2,1,20));
-    parameters.add(position.set("position",ofVec2f(ofGetWidth()*.5,ofGetHeight()*.5),ofVec2f(0,0),ofVec2f(ofGetWidth(),ofGetHeight())));
-    color.set("color",ofColor(127),ofColor(0,0),ofColor(255));
-    parameters.add(color);
 
-    parameters.add(bars_min_width.set("bar width min",0,100,500));
-    parameters.add(bars_max_width.set("bar width max",0,100,500));
+    generalGUI.setName("General Params");
+    generalGUI.add(minNoiseCorner.set("min Noise Corner", 0,10,100));
+    generalGUI.add(maxNoiseCorner.set("max Noise Corne", 0,10,100));
+    
+    gradientGUI.setName("Gradient");
+    
+    gradientGUI.add(gradient_min_width.set("min Width",0, 10,200));
+    gradientGUI.add(gradient_max_width.set("max Width",0, 10,200));
+    gradientGUI.add(gradient_lower_color.set("lower Color",ofColor(127),ofColor(0,0),ofColor(255)));
+    gradientGUI.add(gradient_higher_color.set("higher Color", ofColor(127),ofColor(0,0),ofColor(255)));
+
+
+    
+    barsGUI.setName("Bars");
+    barsGUI.add(bars_min_width.set("bar width min",0,10,300));
+    barsGUI.add(bars_max_width.set("bar width max",0,10,300));
+    
+    spikesGUI.setName("Spikes");
+    spikesGUI.add(minSpikesRandPoints.set("min Rand Points", 0,3,10));
+    spikesGUI.add(maxSpikesRandPoints.set("max Rand Points", 0,20,50));
+    spikesGUI.add(spikes_min_num.set("min num",0,0,20));
+    spikesGUI.add(spikes_max_num.set("max num",0,0,20));
+
 
 
 }
@@ -88,6 +103,54 @@ void ofMorphRender::draw_bar(ofMorph m, int screen_i) {
 }
 
 void ofMorphRender::draw_spikes(ofMorph m, int screen_i) {
+    
+    ofFbo *screen;
+    float scaleH, posx, posy = CHEIGHT/2;
+    
+    switch (screen_i) {
+        case 0:
+            scaleH = ofMap(m.x, kinect_width, 0, 0.5, 3);
+            posx = ofMap(m.y, kinect_height, 0, 0, CWIDTH1);
+            screen = screen1;
+            break;
+        case 1:
+            scaleH = ofMap(m.y, kinect_height, 0, 0.5, 3);
+            posx = ofMap(m.x, 0, kinect_width, 0, CWIDTH2);
+            screen = screen2;
+            break;
+        case 2:
+            scaleH = ofMap(m.x, 0, kinect_width, 0, 3, 0.5);
+            posx = ofMap(m.x, 0, kinect_width, 0, 0.5, 3);
+            screen = screen3;
+            break;
+    }
+    
+    
+
+    screen->begin();
+    
+    ofClear(255,255,255);
+    
+    ofPushStyle();
+    ofPushMatrix();
+    ofTranslate(posx, posy);
+    ofScale(scaleH, scaleH);
+    ofSetColor(0,0,0);
+    ofFill();
+        ofPushMatrix();
+            ofBeginShape();
+            ofVertex(-m.w/2 + m.random_delta[0], -m.h/2 + m.random_delta[1]);
+            ofVertex(m.w/2 + m.random_delta[2], -m.h/2 + m.random_delta[3]);
+            ofVertex(m.w/2 + m.random_delta[4], m.h/2 + m.random_delta[5]);
+            ofVertex(-m.w/2 + m.random_delta[6], m.h/2 + m.random_delta[7]);
+            ofEndShape();
+        ofPopMatrix();
+    ofPopMatrix();
+    ofPopStyle();
+    
+
+    screen->end();
+    
     
 }
 
@@ -167,7 +230,7 @@ void ofMorphRender::draw_gradient(ofMorph m, int screen_i) {
 
 void ofMorphRender::addMorph(float x, float y, int id){
     ofMorph *m = new ofMorph();
-    m->setup(x, y);
+    m->setup(x, y, minSpikesRandPoints, maxSpikesRandPoints, minNoiseCorner, maxNoiseCorner);
     morphs[id] = *m;
 }
 
